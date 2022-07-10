dccb4945dbc57d36c946787ceb0f8c4c2c552d1e
lovergirll <ftm.ahmadi11228@gmail.com>
2022-07-10 22:21:12 +0430

    test2

---
+++ main2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace sth;
+int main()
+{
+	float scor [13],sum=0,ave=0;
+	for(inti=0;i<13;i++)
+	{
+		cout<<"inter your score";
+		cin>>score[i];
+		sum+=score[i];
+	}
+	ave=sum/13;
+	if(ave>=12);
+	{
+		cout<<"pass";
+	}
+	else
+	{
+		cout<<"fail";
+	}
+	return0;
+}
